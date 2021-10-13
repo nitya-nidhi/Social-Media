b953631b622fae1c6804e3834e609882e3a32c7c
nitya-nidhi <nityanidhi05@gmail.com>
2021-10-13 15:05:10 +0530

    first Commit

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,52 @@
+/**
+    * @description      : 
+    * @author           : Administrator
+    * @group            : 
+    * @created          : 13/10/2021 - 13:09:28
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 13/10/2021
+    * - Author          : Administrator
+    * - Modification    : 
+**/
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+export default function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Nitya Nidhi</h4>
+                <span className="profileInfoDesc">Hello my friends!</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
